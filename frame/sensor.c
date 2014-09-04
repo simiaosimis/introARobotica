f5e0926d3686222f5dcec4851cf32795ff17e2d4
cotrim <cotrim149@gmail.com>
2014-09-04 00:32:56 -0300

    add funcoes para sensor de toque

--- frame/sensor.c
+++ frame/sensor.c
@@ -1,7 +1,56 @@
 
-#include <stdio.h>
 // Fazer retirada das bibliotecas acima
 #include "sensor.h"
 
+
+int setUpTouchSensor(const byte &port1,const byte &port2,int qtd,int *resultSensor1, int *resultSensor2){
+	
+	if(qtd == 1){
+		SetSensorTouch(port1);
+		*resultSensor1=1;
+		*resultSensor2=0;
+	}else if(qtd == 2){
+		SetSensorTouch(port1);
+		SetSensorTouch(port2);
+		*resultSensor1=1;
+		*resultSensor2=1;
+
+	} else{
+		*resultSensor1 =-1;		 
+		*resultSensor2 =-1;
+		return 0;
+	}
+	
+	return 1;
+}
+
+/*
+	result1 and result2 = -1 => qtd is not equal to 1 or 2
+*/
+
+void touchSensor(const byte &port1,const byte &port2,int qtd,int *resultSensor1, int *resultSensor2){
+
+
+	if(setUpTouchSensor(port1,port2,qtd,resultSensor1,resultSensor2) == 0){
+		return;
+	}
+		
+/*
+	Exemple
+		
+	while(true){
+		// Sensor 1 or 2 touch
+		if(Sensor(port1) == 1 || Sensor(port2) == 1){
+			TextOut(0,0,"Sensor tocou!");
+			// implementar o que quiser aqui, mudar retorno caso precise
+		}
+		else{
+			TextOut(0,0,"Sensor nao tocou!");			
+			// implementar o que quiser aqui
+
+		}
+*/
+
+}
 
 
--- frame/sensor.h
+++ frame/sensor.h
@@ -1,4 +1,7 @@
 #ifndef _SENSOR_H
 #define _SENSOR_H
 
+int setUpTouchSensor(const byte &port1,const byte &port2,int qtd,int &resultSensor1, int &resultSensor2);
+void touchSensor(const byte &port1,const byte &port2,int qtd,int &resultSensor1, int &resultSensor2);
+
 #endif
